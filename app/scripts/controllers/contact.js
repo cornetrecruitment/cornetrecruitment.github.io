a87474d8c2118f09c15a122499f02d47a862678b
nl-hugo <info@hugojanssen.nl>
2016-01-25 08:04:53 +0100

    Add contact page

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cornetrecruitmentgithubioApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the cornetrecruitmentgithubioApp
+ */
+angular.module('cornetrecruitmentgithubioApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
